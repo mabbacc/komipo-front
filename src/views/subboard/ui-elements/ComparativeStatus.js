02d7dfaab54ecd0b4e00b3ae9e6abce6689a93dc
이경진 <mabbacc@k1ple.com>
2022-03-11 11:12:09 +0900

    샘플 데이터 생성 방법

--- src/views/subboard/ui-elements/ComparativeStatus.js
+++ src/views/subboard/ui-elements/ComparativeStatus.js
@@ -1,19 +1,7 @@
-import { Fragment, useState, useEffect } from 'react'
-import { Col, Row } from 'reactstrap'
+import { Fragment } from "react"
+import { Col, Row } from "reactstrap"
 import Chart from 'react-apexcharts'
-import { generateDayWiseTimeSeries } from '@utils'
 
-const ComparativeStatus = () => {
-  /*TODO: delete this line : Test Code for generate sample Data */
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-      setData(
-            generateDayWiseTimeSeries(new Date('11 Fab 2022').getTime(), 185, {
-              min: 30,
-              max: 90
-            }))
-  }, [])
 
 const ComparativeStatus = () => {   
     const options = {
@@ -71,19 +59,6 @@ const ComparativeStatus = () => {
             </Row>
         </Fragment>
     )
-  }, [])
-
-  return (
-    <Fragment>
-      <Row>
-        <Col></Col>
-      </Row>
-      {
-        /*TODO: delete this block : Test Code for generate sample Data */
-        data.length > 1 ? data[2] : 'null'
-      }
-    </Fragment>
-  )
 }
 
 export default ComparativeStatus
