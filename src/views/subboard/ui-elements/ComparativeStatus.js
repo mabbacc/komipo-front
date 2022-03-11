b71dd98bbd3b28283c3d6a53897257fe71da72c3
이경진 <mabbacc@k1ple.com>
2022-03-11 10:54:45 +0900

    Sample Data for Sample Code

--- src/views/subboard/ui-elements/ComparativeStatus.js
+++ src/views/subboard/ui-elements/ComparativeStatus.js
@@ -1,17 +1,33 @@
-import { Fragment } from "react"
-import { Col, Row } from "reactstrap"
+import { Fragment, useState, useEffect } from 'react'
+import { Col, Row } from 'reactstrap'
 import Chart from 'react-apexcharts'
+import { generateDayWiseTimeSeries } from '@utils'
 
+const ComparativeStatus = () => {
+  /*TODO: delete this line : Test Code for generate sample Data */
+  const [data, setData] = useState([])
 
-const ComparativeStatus = () => {          
-      return (
-          <Fragment>
-            <Row>
-                <Col>
-                </Col>
-            </Row>
-        </Fragment>
+  useEffect(() => {
+    /*TODO: delete this line : Test Code for generate sample Data */
+    setData(
+      generateDayWiseTimeSeries(new Date('11 Fab 2022').getTime(), 185, {
+        min: 30,
+        max: 90
+      })
     )
+  }, [])
+
+  return (
+    <Fragment>
+      <Row>
+        <Col></Col>
+      </Row>
+      {
+        /*TODO: delete this block : Test Code for generate sample Data */
+        data.length > 1 ? data[2] : 'null'
+      }
+    </Fragment>
+  )
 }
 
 export default ComparativeStatus
